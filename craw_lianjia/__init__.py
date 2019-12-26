f34cf831a437548f1f986bbc2e544eb1b1cde2c4
double-point <1010490079@qq.com>
2019-12-26 20:49:35 +0800

    爬取链家网租房数据

---
+++ craw_lianjia/__init__.py
@@ -0,0 +1,10 @@
+# encoding:utf-8
+# FileName: __init__.py
+# Author:   wzg
+# email:    1010490079@qq.com
+# Date:     2019/12/24 0:04
+# Description: 爬取并分析链家租房数据
+
+
+if __name__ == '__main__':
+    pass
