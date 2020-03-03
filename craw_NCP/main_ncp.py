aaf953d1341f7103b31122c9d2791bf75fcd65d1
double-point <1010490079@qq.com>
2020-03-03 10:05:31 +0800

    针对官网北京市当前确诊数据异常的更新

--- craw_NCP/main_ncp.py
+++ craw_NCP/main_ncp.py
@@ -22,7 +22,7 @@ from craw_NCP.preprocess_data import process_data, save_to_mysql, compare_data, 
 # 显示所有列
 pd.set_option('display.max_columns', None)
 # 显示所有行
-# pd.set_option('display.max_rows', None)
+pd.set_option('display.max_rows', None)
 
 if __name__ == '__main__':
     path_dir = r'D:\note\data_source\ncp_data'
@@ -44,6 +44,9 @@ if __name__ == '__main__':
     # ①清洗数据
     df_city_data = process_data(df_city_data, data_time_str, 'city')
     df_province_data = process_data(df_province_data, data_time_str, 'province')
+    # 异常数据进行替换
+    df_city_data['curr_diagnose'][df_city_data['curr_diagnose'] == '-'] = -1
+
     # ②保存数据到数据库中
     save_to_mysql(df_city_data, 'city')
     save_to_mysql(df_province_data, 'province')
@@ -55,9 +58,10 @@ if __name__ == '__main__':
     # ②比较数据
     df_city_result, df_plot_city_data = compare_data(df_city_data)
     df_province_result, df_plot_province_data = compare_data(df_province_data)
+
     # ③各省的热力地图
     filepath_save = os.path.join(path_dir, data_time_str + '_visualmap.png')
-    plot_map(df_plot_province_data, '截止' + data_time_str + '中国区累积确认病例分布地图', filepath_save)
+    plot_map(df_plot_province_data, '截止' + data_time_str + '中国区当前确认病例分布地图  (by:『知秋小梦』)', filepath_save)
     # ④对列名进行相关更改，方便邮件显示
     df_city_result = rename_df(df_city_result, 'city')
     df_province_result = rename_df(df_province_result, 'province')
@@ -74,4 +78,5 @@ if __name__ == '__main__':
             img_list.append(img_path)
     # 发送邮件
     send_email(data_time_str, img_list, df_city_result, df_province_result)
-    exit()
+    # 系统退出
+    sys.exit(1)
